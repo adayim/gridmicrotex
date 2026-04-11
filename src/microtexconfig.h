7157ef35b62c83bec826842c9e1d0e68140634c2
Alim Dayim <alimdayim@hotmail.com>
2026-04-11 01:20:08 +0100

    Fix warning again

--- src/microtexconfig.h
+++ src/microtexconfig.h
@@ -2,16 +2,6 @@
 
 // Generated manually for R package build (normally from microtexconfig.h.in via CMake)
 
-// Suppress warnings in vendored MicroTeX code that we cannot modify.
-// This header is force-included via -include microtexconfig.h in Makevars.
-#if defined(__clang__)
-#pragma clang diagnostic ignored "-Wmismatched-tags"
-#pragma clang diagnostic ignored "-Wunused-but-set-variable"
-#pragma clang diagnostic ignored "-Wmissing-braces"
-#elif defined(__GNUC__)
-#pragma GCC diagnostic ignored "-Wreorder"
-#endif
-
 // Disable auto font find - we load fonts explicitly
 // #define HAVE_AUTO_FONT_FIND
 
